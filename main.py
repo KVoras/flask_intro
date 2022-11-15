cda8fed01a0efe2f5d55ea08bb4d1bf4493630e4
Kestutis Voras <kestas.voras@gmail.com>
2022-11-15 20:07:46 +0200

    Flask Intro

--- main.py
+++ main.py
@@ -6,3 +6,14 @@
 -render_template
 -extend_template
 """
+from flask import Flask, request, render_template
+app = Flask(__name__)
+@app.route("/", methods=['GET', 'POST']) #decorator
+def hello_world():
+    print(request.method)
+    print(request.form.get('username'))
+    return render_template('form.html')
+
+app.run()
+
+
